7544fef40b7f6c4be9cbd7a51a36d4edd3ae4aa1
hildakim <pinkredjohn@gmail.com>
2021-11-02 15:54:13 +0900

    Create Map
    api 이용, 지도에 키워드 검색한 장소를 marker와 목록으로 표시

--- roulette/views.py
+++ roulette/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def map(request):
+    #food : 룰렛에서 받아온 메뉴명
+    # food = request.GET.get('food') 
+    food = '찌개'
+    return render(request, 'map.html', {'food': food})
