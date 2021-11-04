b4a6355843c8f63a82ebc40bfcf2ec7a033b5f02
jay <jaewhan232@naver.com>
2021-11-04 15:13:57 +0900

    각 카테고리별 메뉴 룰렛 수정

--- roulette/views.py
+++ roulette/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def mainpage(request):
-    return render(request, "menu.html")
+    return render(request, "roulette.html")
 
 def menu_roulette(request, menu_id):
-    menu = ['fastfood', 'westernfood', 'koreanfood', 'chinesefood', 'japanesefood']
-    urls = menu[menu_id] + ".html"
+    menu = ['korean','chinese','japanese','western','fastfood']
+    urls = menu[menu_id] + "_roulette.html"
     return render(request, urls)
 
 def map(request, food):
     #food : 룰렛에서 받아온 메뉴명
     menu = food
-    #food = '찌개'
+    
     return render(request, 'map.html', {'menu': menu})
 
