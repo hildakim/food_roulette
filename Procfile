b7686d0e7503dc29fed5b311bd5df2fe12595b00
hildakim <pinkredjohn@gmail.com>
2021-11-06 18:48:52 +0900

    Ready to release
    - 배포 준비

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn food_roulette.wsgi --log-file -
--- food_roulette/settings.py
+++ food_roulette/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zg!btw1_$49o3!5rofsafp&%6!9-48!j=h#^&bo_z^!ck0-p(r'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-zg!btw1_$49o3!5rofsafp&%6!9-48!j=h#^&bo_z^!ck0-p(r')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,3 +136,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+# Heroku: Update database configuration from $DATABASE_URL. 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500) 
+DATABASES['default'].update(db_from_env)
